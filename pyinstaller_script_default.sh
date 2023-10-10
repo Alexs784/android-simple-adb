66f2bc3bbd6cd8f98ac930652b04c3b25b01bb16
Alessandro Mautone <amautone@canyon.com>
2023-10-10 08:19:01 +0200

    Update release setup

--- pyinstaller_script_default.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf build
-rm -rf dist
-pyinstaller cli.py --name SimpleAdb --add-data "assets/*.png:assets" --hidden-import='sqlalchemy.sql.default_comparator' -F
---
+++ setup.py
@@ -0,0 +1,32 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine-tuning.
+build_options = {
+    'packages': ['kivy', 'sqlalchemy'],
+    'excludes': [],
+    "silent": True
+}
+
+include_files = ['assets']
+
+import sys
+
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+else:
+    base = None
+
+executables = [
+    Executable('main.py', base=base, target_name='SimpleAdb')
+]
+
+setup(
+    name='SimpleAdb',
+    version='0.0.6',
+    description='',
+    options={
+        'build_exe': build_options,
+    },
+    executables=executables,
+)

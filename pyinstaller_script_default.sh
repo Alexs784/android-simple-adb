647a1d985a169c4d65bde67f1df7b0f5c6c7e76e
Alessandro Mautone <amautone@canyon.com>
2024-01-02 20:21:55 +0100

    Update release script to include dependencies

--- pyinstaller_script_default.sh
+++ pyinstaller_script_default.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Install pyinstaller
+pyinstaller_version=$(pyinstaller --version 2>/dev/null)
+if [ "$pyinstaller_version" != "5.9" ]; then
+    pip install pyinstaller==5.9
+fi
+
 rm -rf build
 rm -rf dist
-pyinstaller cli.py --name SimpleAdb --add-data "assets/*.png" -F
+pyinstaller cli.py --name SimpleAdb --add-data "./assets/*.png:assets" -F

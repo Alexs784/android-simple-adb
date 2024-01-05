232ecf55946be57f514b35d67917e5bd2d1a1eb0
Alessandro Mautone <amautone@canyon.com>
2024-01-05 15:39:10 +0100

    Improve retry mechanism

--- comands/commands_utils.py
+++ comands/commands_utils.py
@@ -5,7 +5,7 @@ from sdk_setter.utils import get_adb
 
 def execute_adb_command_getting_result(adb_command, device_id):
     command_to_execute = f"{get_adb()} -s {device_id} {adb_command}"
-    execute_command_getting_result(command_to_execute)
+    return execute_command_getting_result(command_to_execute)
 
 
 def execute_command_getting_result(command):
